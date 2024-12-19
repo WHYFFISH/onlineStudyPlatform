17c2de13c3c46065822404f844fc7b091022f6aa
peerc-pp <Cu2O2@outlook.com>
2024-12-19 09:06:59 +0800

    完善教师页面信息

--- src/app/teacher/upload/page.js
+++ src/app/teacher/upload/page.js
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from 'react';
 import { InboxOutlined } from '@ant-design/icons';
 import { message, Upload, List, Button } from 'antd';
 import styles from './uploadPage.module.css';
+import Image from "next/image";
+import NavigatorMenu from "../../components/navigatorMenu/page";
+import Footer from "../../components/footer/page";
+import logo from "../../../assets/homePage/logo.png"
 
 const { Dragger } = Upload;
 
@@ -75,27 +79,53 @@ export default function CoursewareUpload() {
   };
 
   return (
-    <div className={styles.uploadPage}>
-      <h1 className={styles.title}>课件上传页面</h1>
-      <Dragger {...uploadProps} className={styles.dragger}>
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">点击或拖拽文件到此区域进行上传</p>
-        <p className="ant-upload-hint">支持单个或批量上传，支持所有文件类型</p>
-      </Dragger>
-      <h2 className={styles.listTitle}>已上传的课件</h2>
-      <List
-        bordered
-        dataSource={fileList}
-        renderItem={(item) => (
-          <List.Item>
-            <a href={item.data} target="_blank" rel="noopener noreferrer">
-              {item.name}
-            </a>
-          </List.Item>
-        )}
-      />
+    <div>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <Image className={styles.logoIcon} src={logo} alt="Logo" priority />
+          在线教育平台
+        </div>
+        {/* <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} style={{ width: '390px', fontSize: '16px' }} /> */}
+        <NavigatorMenu initialCurrent={'personal'} />
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Button
+            onClick={() => {
+              localStorage.clear();
+              router.push('/');
+            }}
+            style={{ marginLeft: 60 }}
+          >
+            退出登录
+          </Button>
+
+        </div>
+      </div>
+      <div className={styles.uploadPage}style={{paddingBottom:80}}>
+
+        <h1 className={styles.title}style={{paddingTop:64}}>课件上传页面</h1>
+        <Dragger {...uploadProps} className={styles.dragger}>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">点击或拖拽文件到此区域进行上传</p>
+          <p className="ant-upload-hint">支持单个或批量上传，支持所有文件类型</p>
+        </Dragger>
+        <h2 className={styles.listTitle}>已上传的课件</h2>
+        <List
+          bordered
+          dataSource={fileList}
+          renderItem={(item) => (
+            <List.Item>
+              <a href={item.data} target="_blank" rel="noopener noreferrer">
+                {item.name}
+              </a>
+            </List.Item>
+          )}
+        />
+
+      </div>
+      <Footer />
     </div>
+
   );
 }
